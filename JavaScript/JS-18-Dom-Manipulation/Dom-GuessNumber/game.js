e301eaf5b03df1499920ea82be57d652303a8214
Vicead <obasalioglu@gmail.com>
2023-09-29 12:33:19 +0200

    domAdvance

---
+++ JavaScript/JS-18-Dom-Manipulation/Dom-GuessNumber/game.js
@@ -0,0 +1,18 @@
+//* rasgele sayı tutma işleminde 1 ile 20 arasında bir sayıya ihtiyacımız varsa Math.random() ı 20 ile çarpıp yukarı yuvarlamalıyız
+let PCsayi=Math.ceil(Math.random()*20)
+
+let mesaj=document.querySelector(".msg")
+
+//? her check butonuna basılınca yapılacaklar
+document.querySelector(".check").onclick=()=>{
+    const tahmin=document.querySelector(".guess").value
+    //* tahmin doğruysa
+    if(PCsayi==tahmin){
+        
+    }
+    //* tahmin yanlışsa bizi arttır yada azalt diye yönlendi
+
+}
+
+
+
--- JavaScript/JS-18-Dom-Manipulation/app.js
+++ JavaScript/JS-18-Dom-Manipulation/app.js
@@ -29,6 +29,7 @@ document.querySelector(".konus").onclick=()=>{
 resim.addEventListener("mouseover", ()=>{
     resim.src="./img/aslan2.jpeg"
     document.querySelector(".ses2").play()
+    ses1.pause()
 })
 //! mouse ile resmin üzeriden ayrılınca
 
@@ -41,19 +42,100 @@ resim.addEventListener("mouseout", ()=>{
 const textInput=document.querySelector(".textbox")
 const checkInput=document.querySelector(".checkbox")
 
-// textInput.onkeyup=()=>{
-//     if(checkInput.checked){
-//     textInput.value=textInput.value.toUpperCase()
-//     }else{
-//         textInput.value=textInput.value.toLowerCase()
-//     }
-// }
-
-
-checkInput.addEventListener('change', function() {
+textInput.onkeyup=()=>{
     if(checkInput.checked){
-        textInput.value= textInput.value.toUpperCase()
-    } else {
-        textInput.value= textInput.value.toLowerCase()
+    textInput.value=textInput.value.toUpperCase()
+    }else{
+        textInput.value=textInput.value.toLowerCase()
     }
-});
+}
+
+
+// checkInput.addEventListener('change', function() {
+//     if(checkInput.checked){
+//         textInput.value= textInput.value.toUpperCase()
+//     } else {
+//         textInput.value= textInput.value.toLowerCase()
+//     }
+// });
+
+//! listemin başına "programlaama dilleri" h1 etiketi eklemek ***** uzun yol*****
+
+//? HTML de h1 elementi oluştur 
+const baslik=document.createElement("h1")
+
+//? oluşan h1 elementine class ismi ata
+baslik.className="baslik2"
+
+//? Programlama Dilleri text i oluştur
+const yazi=document.createTextNode("Programlama Dilleri")
+
+//? h1 elementi için oluşturduğum text i h1 e child yapalım
+baslik.appendChild(yazi)
+
+//? input-div class isimli div in son child ı yapalım
+// document.querySelector(".input-div").appendChild(baslik)
+
+//? input-div clas isimli div den hemen sonraya ekle
+// document.querySelector(".input-div").after(baslik)
+
+//!!  "append" metodu ile seçtiğimiz Html etiketi bitmeden önceki son kısma ekleme yapabiliriz (<div>merhaba -buraya-  </div>).  "prepend" metodu ile seçtiğimiz Html etiketi başladıktan sonraki ilk kısma ekleme yapabiliriz  (<div> -buraya-  merhaba   </div>).  "after" metodu ile ile seçtiğimiz Html etiketi bittikten sonraki ilk kısma ekleme yapabiliriz (<div> merhaba </div>  -buraya-). before ile de etiketin önüne ekleyebiliriz
+
+// document.querySelector(".input-div").innerHTML=document.querySelector(".input-div").innerHTML+`<h1 class="title text-danger">Programlama Dilleri</h1`
+
+// document.querySelector(".input-div").innerHTML=`<h1 class="title text-danger">Programlama Dilleri</h1`+document.querySelector(".input-div").innerHTML
+
+
+document.querySelector(".input-div").innerHTML+=`<h1 class="title text-danger mt-4">Programlama Dilleri</h1>`
+
+//! languages inputuna girilen değerleri, ul ye eklemek
+
+//! uzun yol
+
+const dilInput=document.querySelector(".languages")
+
+// //* oluşan yeni li mizi ul nin son child ı yapalım
+const liste=document.querySelector(".liste")
+
+document.querySelector(".ekle").onclick=()=>{
+//     //? inputu js ye çağıralım
+
+
+// //* yeni girilen satırı saklamak için li create edelim
+// const yenili=document.createElement("li")
+
+// //* yenili için text oluşturduk
+// const text=document.createTextNode(dilInput.value)
+
+// //* oluşturduğumuz text i yeni li ye bağladık
+// yenili.appendChild(text)
+
+// liste.appendChild(yenili)
+
+// dilInput.value=""
+
+
+//! kısa yol
+
+liste.innerHTML+=`<li>${dilInput.value}</li>`
+dilInput.value=""
+
+}
+
+//! sil butonuna basılınca ul listesinden li elemanını silmek için removeChild metodunu kullanacağız
+
+
+document.querySelector(".sil").onclick=()=>{
+    liste.removeChild(liste.lastElementChild)
+}
+
+//! klavyedeki keycode ları kullanarak kod yazma 
+
+dilInput.onkeydown=(tus)=>{
+    if(tus.keyCode==13){
+        document.querySelector(".ekle").click()
+    }
+    if(tus.keyCode==46){
+        document.querySelector(".sil").click()
+    }
+}     

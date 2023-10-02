50d8f31ab60939d777e3580345e05f10f8c79b2c
Vicead <obasalioglu@gmail.com>
2023-10-02 14:42:37 +0200

    guessGame

--- JavaScript/JS-18-Dom-Manipulation/Dom-GuessNumber/game.js
+++ JavaScript/JS-18-Dom-Manipulation/Dom-GuessNumber/game.js
@@ -3,16 +3,40 @@ let PCsayi=Math.ceil(Math.random()*20)
 
 let mesaj=document.querySelector(".msg")
 
+let score=document.querySelector(".score")
+
 //? her check butonuna basılınca yapılacaklar
 document.querySelector(".check").onclick=()=>{
     const tahmin=document.querySelector(".guess").value
+    if(!tahmin){
+        mesaj.textContent="Lütfen bir tahmin giriniz"
+    }
     //* tahmin doğruysa
-    if(PCsayi==tahmin){
-        
+    else if(PCsayi==tahmin){
+        mesaj.textContent="Tebrikler bildiniz🎉"
+        document.querySelector("body").style.backgroundColor="green"
+        document.querySelector(".number").textContent=PCsayi;
     }
     //* tahmin yanlışsa bizi arttır yada azalt diye yönlendi
+else{
+    tahmin < PCsayi ? mesaj.textContent="Arttır" : mesaj.textContent="Azalt"
+    if(score.textContent>1){
+        score.textContent--   
+        }else{
+            score.textContent=0
+            mesaj.textContent="oyunu kaybettiniz"
+            document.querySelector("body").style.backgroundColor="red"
+            document.querySelector(".number").textContent=PCsayi
+        }
 
+    }
 }
 
-
-
+document.querySelector(".again").onclick=()=>{
+    PCsayi=Math.ceil(Math.random()*20)
+    document.querySelector("body").style.backgroundColor="#2d3436"
+    mesaj.textContent="oyun yeni oyuncu için başlıyor"
+    document.querySelector(".number").textContent="?"
+    score.textContent=10
+    document.querySelector(".guess").value=""
+}

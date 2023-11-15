7115be319d2ebf2eb417861f0d2f2fb527950f0c
Vicead <obasalioglu@gmail.com>
2023-11-15 11:28:41 +0100

    editButton

--- React/RC-11-UseEffect-axios/src/components/BilgiList.jsx
+++ React/RC-11-UseEffect-axios/src/components/BilgiList.jsx
@@ -1,58 +1,60 @@
-import React from 'react'
-import {FaEdit} from 'react-icons/fa'
-import {AiFillDelete} from 'react-icons/ai'
-import axios from 'axios'
-const BilgiList = ({tutorial,getTutorials}) => {
+import React, { useState } from "react";
+import { FaEdit } from "react-icons/fa";
+import { AiFillDelete } from "react-icons/ai";
+import axios from "axios";
+import EditBilgi from "./EditBilgi";
+const BilgiList = ({ tutorial, getTutorials }) => {
+  const[edit,setEdit]=useState({id:"",title:"",description:""})
   const BASE_URL = "https://tutorial-api.fullstack.clarusway.com/tutorials/";
-  const deleteBilgi=async(id)=>{
-    await axios.delete(`${BASE_URL}${id}/`)
-    getTutorials()
-  }
+  const deleteBilgi = async (id) => {
+    await axios.delete(`${BASE_URL}${id}/`);
+    getTutorials();
+  };
   return (
-    <div className='container mt-4'>
+    <div className="container mt-4">
       <table className="table table-striped">
-   <thead>
-     <tr>
-       <th scope="col">#id</th>
-       <th scope="col">Title</th>
-       <th scope="col">Description</th>
-       <th scope="col" className="text-center">
-         Edit
-       </th>
-     </tr>
-   </thead>
-   <tbody>
-      {tutorial.map(({id,title,description})=>{
-        return(
-          <tr >
-           <th>{id} </th>
-           <td>{title} </td>
-           <td>{description} </td>
-           <td className="text-center ">
-             <AiFillDelete
-                type="button"
-                size={22}
-                className="text-danger cursor-pointer"
-                onClick={()=>deleteBilgi(id)}
-             />
+        <thead>
+          <tr>
+            <th scope="col">#id</th>
+            <th scope="col">Title</th>
+            <th scope="col">Description</th>
+            <th scope="col" className="text-center">
+              Edit
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {tutorial.map(({ id, title, description }) => {
+            return (
+              <tr key={id}>
+                <th>{id} </th>
+                <td>{title} </td>
+                <td>{description} </td>
+                <td className="text-center ">
+                  <AiFillDelete
+                    type="button"
+                    size={22}
+                    className="text-danger cursor-pointer"
+                    onClick={() => deleteBilgi(id)}
+                  />
 
-             <FaEdit
-               data-bs-toggle="modal"
-               data-bs-target="#editModal"
-               size={20}
-               type="button"
-               className="me-2 text-warning cursor-pointer"
-               
-             />
-           </td>
-         </tr>
-        )
-      })}
-      
-    </tbody>
- </table>
+                  <FaEdit
+                    data-bs-toggle="modal"
+                    data-bs-target="#editModal"
+                    size={20}
+                    type="button"
+                    className="me-2 text-warning cursor-pointer"
+                    onClick={()=>setEdit({id,title,description})}
+                  />
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <EditBilgi edit={edit} getTutorials={getTutorials}/>
     </div>
-  )
-}
+  );
+};
 
-export default BilgiList
+export default BilgiList;
--- React/RC-11-UseEffect-axios/src/components/EditBilgi.jsx
+++ React/RC-11-UseEffect-axios/src/components/EditBilgi.jsx
@@ -1,9 +1,96 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
-const EditBilgi = () => {
+const EditBilgi = ({ edit, getTutorials }) => {
+  const BASE_URL = "https://tutorial-api.fullstack.clarusway.com/tutorials/";
+  const { id, title: oldTitle, description: oldDesc } = edit;
+  const [title, setTitle] = useState(oldTitle);
+  const [desc, setDesc] = useState(oldDesc);
+  useEffect(() => {
+    setTitle(oldTitle);
+    setDesc(oldDesc);
+  }, [oldTitle, oldDesc]);
+  const putInfo = async (data) => {
+    try {
+      await axios.put(`${BASE_URL}${id}/`, data);
+    } catch (error) {
+    } finally {
+      getTutorials();
+    }
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    putInfo({ title, description: desc });
+  };
+
   return (
-    <div>EditBilgi</div>
-  )
-}
+    <div>
+      <div
+        className="modal fade"
+        id="editModal"
+        tabIndex={-1}
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5" id="exampleModalLabel">
+                Edit Tutorial
+              </h1>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              />
+            </div>
+            <div className="modal-body">
+              <form onSubmit={handleSubmit}>
+                <div className="mb-3">
+                  <label htmlFor="title" className="form-label">
+                    Title
+                  </label>
+                  <input
+                    type="text"
+                    id="title"
+                    className="form-control"
+                    placeholder="Enter your title"
+                    required
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                  />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="description" className="form-label">
+                    Description
+                  </label>
+                  <input
+                    type="text"
+                    id="Description"
+                    className="form-control"
+                    placeholder="Enter your description"
+                    required
+                    value={desc}
+                    onChange={(e) => setDesc(e.target.value)}
+                  />
+                </div>
+                <div className="modal-footer">
+                  <button
+                    type="submit"
+                    className="btn btn-primary"
+                    data-bs-dismiss="modal"
+                  >
+                    Save
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default EditBilgi
+export default EditBilgi;

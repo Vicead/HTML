7889ab8c787ce5bddb54d378eec49aa4dfe98acf
Vicead <obasalioglu@gmail.com>
2023-11-21 13:00:28 +0100

    shoppingCart

---
+++ React/RC-14-Shopping-Card/src/pages/ProductList.jsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from "react";
+import ProductCard from "../components/ProductCard";
+import CardTotal from "../components/CardTotal";
+import axios from "axios";
+
+const ProductList = () => {
+ const[products,setProducts]=useState([])
+ const[error,setError]=useState(false)
+ const[loading,setLoading]=useState(true)
+
+ const BASE_URL="https://63f4e5583f99f5855db9e941.mockapi.io/products"
+ const getData=async()=>{ 
+  try{ 
+  const {data}= await axios(BASE_URL)
+   setProducts(data)
+ }catch(error){
+  setError(true)
+ }finally{
+  setLoading(false)
+  }
+ }
+ useEffect(() => {
+    getData()
+   
+ }, [])
+ 
+if(error){
+  return <p>Something went wrong...</p>;
+} 
+
+  return (
+    <div className="container mt-3">
+      <div className="d-sm-block d-md-flex">
+        {loading===true ? (<p>Loading...</p>):(<>
+            <article id="product-panel" className="col-md-6">
+              {products.map((product)=> (
+                <ProductCard key={product.id} product={product} getData={getData} />
+              ))}
+            </article>
+            <article className="col-md-4 m-3">
+              <CardTotal />
+            </article>
+          </>)}
+          
+        
+      </div>
+    </div>
+  );
+};
+
+export default ProductList;
---
+++ React/RC-14-Shopping-Card/src/pages/UpdateProduct.jsx
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+import ProductForm from "../components/ProductForm";
+import {  useLocation, useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+
+const UpdateProduct = () => {
+  const{state:{product}}=useLocation()
+  const[formData,setFormData]=useState(product)
+  const navigate=useNavigate()
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await axios.put(`https://63f4e5583f99f5855db9e941.mockapi.io/products/${product.id}`,formData);
+    navigate(-1)
+  };
+
+  
+  return (
+    <div className="container">
+      <article
+        name="add-product"
+        className="mb-4 mt-4 col col-lg-6 mx-auto border rounded-2 bg-opacity-50 bg-light"
+      >
+        <h1 className="text-center">update Product</h1>
+        <ProductForm handleSubmit={handleSubmit} formData={formData} setFormData={setFormData} />
+      </article>
+    </div>
+  );
+};
+
+export default UpdateProduct;

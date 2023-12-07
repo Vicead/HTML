a2fb0b80f56fc1a44eb1aab59886022dc711896a
Vicead <obasalioglu@gmail.com>
2023-12-07 12:37:27 +0100

    news

---
+++ React/RC-23-redux-toolkit-example/src/features/yetkiSlice.jsx
@@ -0,0 +1,34 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+
+
+const yetkiSlice = createSlice({
+ name:"yetkiSlice",
+
+ initialState:{
+    email:"",
+    password:""
+ },
+
+reducers:{
+
+    olusturKullanici:(state,action)=>{
+state.email=action.payload.email
+state.password=action.payload.password
+
+    },
+
+    kullaniciSil:(state)=>{
+
+    }
+}
+
+
+
+
+
+})
+
+export const {olusturKullanici,kullaniciSil} =yetkiSlice.actions
+
+export default yetkiSlice.reducer

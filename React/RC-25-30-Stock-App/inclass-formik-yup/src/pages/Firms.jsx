80f7a2d9980564ae80e7be42dbd26fd42d6e8ad0
Vicead <obasalioglu@gmail.com>
2023-12-15 12:34:16 +0100

    firms map

--- React/RC-25-30-Stock-App/inclass-formik-yup/src/pages/Firms.jsx
+++ React/RC-25-30-Stock-App/inclass-formik-yup/src/pages/Firms.jsx
@@ -2,7 +2,12 @@ import React, { useEffect } from "react";
 import axios from "axios";
 import { useSelector } from "react-redux";
 import useStockCall from "../hooks/useStockCall";
+// import Container from "@mui/material/Container" //! bu yöntem daha performanslı
+import { Button, Container, Grid, Typography } from "@mui/material"; //! birden fazla olduğunda yazım kolaylığı sağlıyor.
+import FirmCard from "../components/Cards/FirmsCard";
+
 const Firms = () => {
+  //? firms verileri bana birden fazla yerde lazım olduğu için fonksiyonu burada değil de her yerden erişebileceğim bir noktada tanımlıyorum. İçerisinde react hookları lazım olduğu için de bu ortak nokta en iyi custom hook olmuş oluyor.
   // const BASE_URL = import.meta.env.VITE_BASE_URL
   // const {token} = useSelector(state=>state.auth)
   // const getFirms = async ()=>{
@@ -24,10 +29,29 @@ const Firms = () => {
 
   useEffect(() => {
     // getFirms();
-    getStockData("firms")
+    getStockData("firms");
   }, []);
   console.log(firms);
-  return <div style={{ marginTop: "50rem" }}>Firms</div>;
+  return (
+    <Container maxWidth={"xl"}>
+      <Typography
+        align="center"
+        variant="h4"
+        component={"h1"}
+        color={"secondary.second"}
+      >
+        Firms
+      </Typography>
+      <Button variant="contained">New Firm</Button>
+      <Grid container spacing={2} mt={3}>
+        {firms?.map((firm) => (
+          <Grid key={firm._id} item xs={12} md={6} lg={4} xl={3}>
+            <FirmCard {...firm} />
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+  );
 };
 
 export default Firms;

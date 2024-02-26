be4f2e83b05174671fc0ab34ff40669cc84242c4
Vicead <obasalioglu@gmail.com>
2024-02-26 12:39:33 +0100

    PizzaApi

---
+++ nodeJs/class-notes/19-PizzaAPI/src/models/user.js
@@ -0,0 +1,50 @@
+"use strict"
+/* -------------------------------------------------------
+    NODEJS EXPRESS | CLARUSWAY FullStack Team
+------------------------------------------------------- */
+const { mongoose } = require('../configs/dbConnection')
+/* ------------------------------------------------------- *
+{
+    "username": "test",
+    "password": "1234",
+    "email": "abc@site.com",
+    "isAdmin": "true"
+}
+/* ------------------------------------------------------- */
+// User Model:
+
+const passwordEncrypt = require('../helpers/passwordEncrypt')
+
+const UserSchema = new mongoose.Schema({
+
+    username: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true,
+    },
+
+    password: {
+        type: String,
+        trim: true,
+        required: true,
+        set: (password) => passwordEncrypt(password)
+    },
+
+    isActive: {
+        type: Boolean,
+        default: true,
+    },
+
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
+
+}, {
+    collection: 'users',
+    timestamps: true
+})
+
+/* ------------------------------------------------------- */
+module.exports = mongoose.model('User', UserSchema)
